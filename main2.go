770d18ee42b95eb8d8156329716d5021b38e0e2f
Ase Deliri <deliri.ase@gmail.com>
2017-03-08 10:53:34 -0500

    changed app engine instance

---
+++ main2.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/gzip"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Set Gin to production mode
+	// gin.SetMode(gin.ReleaseMode)
+	// Get the gin router
+	r := gin.Default()
+	// add gzip to load files faster
+	r.Use(gzip.Gzip(gzip.DefaultCompression))
+	// parse and execute templates
+	r.LoadHTMLGlob("staging/*.gohtml")
+	// grab static components
+	r.Static("/assets", "./assets")
+	// control home route
+	r.GET("/", Index)
+	// Handle all other routes
+	r.NoRoute(NotFound)
+
+	// To run locally uncomment the line below and change init func to main
+	r.Run(":3000")
+
+	// To work with App Engine
+	http.Handle("/", r)
+
+}
+
+// Index loads the site
+func Index(c *gin.Context) {
+	c.HTML(200, "resume.gohtml", gin.H{
+		"title": "Deliri Resume",
+	})
+}
+
+// NotFound handles improper route requests
+func NotFound(c *gin.Context) {
+	c.HTML(404, "404.gohtml", gin.H{
+		"title": "404",
+	})
+	return
+}
+
+
+./letsencrypt-auto certonly -a manual --rsa-key-size 2048 -d deliri.me -d www.deliri.me
